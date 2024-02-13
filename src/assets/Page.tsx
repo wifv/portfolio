c5bea96ff7a657219ce842c6fd6bf7bead86831e
Abduaziz <abcogenshin@gmail.com>
2024-02-13 20:56:54 +0500

    about me?

--- src/assets/Page.tsx
+++ src/assets/Page.tsx
@@ -19,11 +19,13 @@ const Page = () => {
         <img src="/src/assets/me.jpg" alt="my image" className="my-image "/>
       </section>
       <section className="about-me">
-        <h1 className="text-center">About Me</h1>
+        <h1 className="text-center h1">About Me</h1>
         <h5 className="text-center">My Introduction</h5>
         <br /><br /><br />
         <div className="me-block">
-            <img src="/src/assets/me.jpg" alt="my image" className="my-image my-image-2"/>
+            <div className="some-container">
+              <img src="/src/assets/also_me.jpg" alt="my image" className="my-image my-image-2"/>
+            </div>
             <div className="me-right">
                 <div className="me-right-block">
                     <div className="me-right-1">
@@ -42,14 +44,91 @@ const Page = () => {
                         <div>Online 24/7</div>
                     </div>
                 </div>
-                <p className="me-right-block">
-                    Creating websites of variout complexity. <br />
-                    Online shopping, Game catalog, Audio player, Beautiful <br />
-                    Designs and maybe a Weather app? <br />
-                    Teamwork is my second name, and i google quite a lot <br />
+                <p className="me-right-block color-2">
+                    Creating websites of various complexity. <br />
+                    Online shopping, Game catalog, Audio player, <br />
+                    Beautiful Designs and maybe a Weather app? <br />
+                    Teamwork is my second name, and i google <br /> quite a lot
+                    ching chong ching chong ching chong <br />
+                    ching chong ching chong ching chong <br />
+                    ching chong ching chong ching chong <br />
                 </p>
-                <a href="#"><div className="hello-btn">Contact Me</div></a>
+                <a href="#"><div className="hello-btn">
+                  Contact Me
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#fff"><path d="M15.25 22.7502H9.25C3.82 22.7502 1.5 20.4302 1.5 15.0002V9.00024C1.5 3.57024 3.82 1.25024 9.25 1.25024H14.25C14.66 1.25024 15 1.59024 15 2.00024C15 2.41024 14.66 2.75024 14.25 2.75024H9.25C4.64 2.75024 3 4.39024 3 9.00024V15.0002C3 19.6102 4.64 21.2502 9.25 21.2502H15.25C19.86 21.2502 21.5 19.6102 21.5 15.0002V10.0002C21.5 9.59024 21.84 9.25024 22.25 9.25024C22.66 9.25024 23 9.59024 23 10.0002V15.0002C23 20.4302 20.68 22.7502 15.25 22.7502Z" fill="var(--container-color)"></path><path d="M22.25 10.7502H18.25C14.83 10.7502 13.5 9.42023 13.5 6.00023V2.00023C13.5 1.70023 13.68 1.42023 13.96 1.31023C14.24 1.19023 14.56 1.26023 14.78 1.47023L22.78 9.47023C22.99 9.68023 23.06 10.0102 22.94 10.2902C22.82 10.5702 22.55 10.7502 22.25 10.7502ZM15 3.81023V6.00023C15 8.58023 15.67 9.25023 18.25 9.25023H20.44L15 3.81023Z" fill="var(--container-color)"></path><path d="M13.25 13.7502H7.25C6.84 13.7502 6.5 13.4102 6.5 13.0002C6.5 12.5902 6.84 12.2502 7.25 12.2502H13.25C13.66 12.2502 14 12.5902 14 13.0002C14 13.4102 13.66 13.7502 13.25 13.7502Z" fill="var(--container-color)"></path><path d="M11.25 17.7502H7.25C6.84 17.7502 6.5 17.4102 6.5 17.0002C6.5 16.5902 6.84 16.2502 7.25 16.2502H11.25C11.66 16.2502 12 16.5902 12 17.0002C12 17.4102 11.66 17.7502 11.25 17.7502Z" fill="whit#fff"></path></svg>
+                </div></a>
             </div>
+        </div>
+      </section>
+
+      <section className="Skills margin-top">
+        <h1 className="text-center">Skills</h1>
+        <h5 className="text-center color-2">My Technical Level</h5>
+        
+        <div className="skills">
+          <div className="skill-block">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M256 48a208 208 0 1 1 0 416 208 208 0 1 1 0-416zm0 464A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM369 209c9.4-9.4 9.4-24.6 0-33.9s-24.6-9.4-33.9 0l-111 111-47-47c-9.4-9.4-24.6-9.4-33.9 0s-9.4 24.6 0 33.9l64 64c9.4 9.4 24.6 9.4 33.9 0L369 209z"/></svg>
+            <div className="skill-block-right">
+              <h5>HTML</h5>
+              <h5>Intermediate</h5>
+            </div>
+          </div>
+          <div className="skill-block">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M256 48a208 208 0 1 1 0 416 208 208 0 1 1 0-416zm0 464A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM369 209c9.4-9.4 9.4-24.6 0-33.9s-24.6-9.4-33.9 0l-111 111-47-47c-9.4-9.4-24.6-9.4-33.9 0s-9.4 24.6 0 33.9l64 64c9.4 9.4 24.6 9.4 33.9 0L369 209z"/></svg>
+            <div className="skill-block-right">
+              <h5>CSS</h5>
+              <h5>Intermidiate</h5>
+            </div>
+          </div>
+          <div className="skill-block">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M256 48a208 208 0 1 1 0 416 208 208 0 1 1 0-416zm0 464A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM369 209c9.4-9.4 9.4-24.6 0-33.9s-24.6-9.4-33.9 0l-111 111-47-47c-9.4-9.4-24.6-9.4-33.9 0s-9.4 24.6 0 33.9l64 64c9.4 9.4 24.6 9.4 33.9 0L369 209z"/></svg>
+            <div className="skill-block-right">
+              <h5>Git</h5>
+              <h5>Basic</h5>
+            </div>
+          </div>
+          <div className="skill-block">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M256 48a208 208 0 1 1 0 416 208 208 0 1 1 0-416zm0 464A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM369 209c9.4-9.4 9.4-24.6 0-33.9s-24.6-9.4-33.9 0l-111 111-47-47c-9.4-9.4-24.6-9.4-33.9 0s-9.4 24.6 0 33.9l64 64c9.4 9.4 24.6 9.4 33.9 0L369 209z"/></svg>
+            <div className="skill-block-right">
+              <h5>Sass</h5>
+              <h5>Intermediate</h5>
+            </div>
+          </div>
+          <div className="skill-block">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M256 48a208 208 0 1 1 0 416 208 208 0 1 1 0-416zm0 464A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM369 209c9.4-9.4 9.4-24.6 0-33.9s-24.6-9.4-33.9 0l-111 111-47-47c-9.4-9.4-24.6-9.4-33.9 0s-9.4 24.6 0 33.9l64 64c9.4 9.4 24.6 9.4 33.9 0L369 209z"/></svg>
+            <div className="skill-block-right">
+              <h5>React</h5>
+              <h5>Basic</h5>
+            </div>
+          </div>
+          <div className="skill-block">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M256 48a208 208 0 1 1 0 416 208 208 0 1 1 0-416zm0 464A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM369 209c9.4-9.4 9.4-24.6 0-33.9s-24.6-9.4-33.9 0l-111 111-47-47c-9.4-9.4-24.6-9.4-33.9 0s-9.4 24.6 0 33.9l64 64c9.4 9.4 24.6 9.4 33.9 0L369 209z"/></svg>
+            <div className="skill-block-right">
+              <h5>tailwind</h5>
+              <h5>Basic</h5>
+            </div>
+          </div>
+          <div className="skill-block">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M256 48a208 208 0 1 1 0 416 208 208 0 1 1 0-416zm0 464A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM369 209c9.4-9.4 9.4-24.6 0-33.9s-24.6-9.4-33.9 0l-111 111-47-47c-9.4-9.4-24.6-9.4-33.9 0s-9.4 24.6 0 33.9l64 64c9.4 9.4 24.6 9.4 33.9 0L369 209z"/></svg>
+            <div className="skill-block-right">
+              <h5>Bootstrap</h5>
+              <h5>Basic</h5>
+            </div>
+          </div>
+          <div className="skill-block">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M256 48a208 208 0 1 1 0 416 208 208 0 1 1 0-416zm0 464A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM369 209c9.4-9.4 9.4-24.6 0-33.9s-24.6-9.4-33.9 0l-111 111-47-47c-9.4-9.4-24.6-9.4-33.9 0s-9.4 24.6 0 33.9l64 64c9.4 9.4 24.6 9.4 33.9 0L369 209z"/></svg>
+            <div className="skill-block-right">
+              <h5>JavaScript</h5>
+              <h5>Intermediate</h5>
+            </div>
+          </div>
+          <div className="skill-block">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M256 48a208 208 0 1 1 0 416 208 208 0 1 1 0-416zm0 464A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM369 209c9.4-9.4 9.4-24.6 0-33.9s-24.6-9.4-33.9 0l-111 111-47-47c-9.4-9.4-24.6-9.4-33.9 0s-9.4 24.6 0 33.9l64 64c9.4 9.4 24.6 9.4 33.9 0L369 209z"/></svg>
+            <div className="skill-block-right">
+              <h5>TypeScript</h5>
+              <h5>Intermediate</h5>
+            </div>
+          </div>
         </div>
       </section>
     </div>
