116ef2aa6b1842599ef037b6bf9368656ddab061
wifv <abcogenshin@gmail.com>
2024-02-18 20:43:49 +0500

    navbar

--- src/assets/Navbar.tsx
+++ src/assets/Navbar.tsx
@@ -6,11 +6,11 @@ const Navbar = () => {
       <nav className="nav">
           <a className="logo" href='/'>My Portfolio</a>
           <ul>
-              <li className="nav-item"><a href='#'>About</a></li>
-              <li className="nav-item"><a href='#'>Skills</a></li>
-              <li className="nav-item"><a href='#'>Service</a></li>
-              <li className="nav-item"><a href='#'>Projects</a></li>
-              <li className="nav-item"><a href='#'>Contact</a></li>
+              <li className="nav-item"><a href='#about'>About</a></li>
+              <li className="nav-item"><a href='#skills'>Skills</a></li>
+              {/* <li className="nav-item"><a href='#'>Service</a></li> */}
+              <li className="nav-item"><a href='#projects'>Projects</a></li>
+              <li className="nav-item"><a href='#contact'>Contact</a></li>
           </ul>
       </nav>
     </div>
--- src/assets/Projects.tsx
+++ src/assets/Projects.tsx
@@ -4,33 +4,54 @@ import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
+import './projects.css';
+import './global.css';
 
 
 const Projects = () => {
   return (
-    <div>
+    <div className='projects'>
       <Swiper
       modules={[Navigation, Pagination, Scrollbar, A11y]}
-      spaceBetween={50}
+      spaceBetween={30}
       slidesPerView={3}
-      navigation
       pagination={{ clickable: true }}
       scrollbar={{ draggable: true }}
       >
         <SwiperSlide className='slide'>
           <img src="src/assets/rawg.png" alt="" />
+          <div className="info-container">
+            <h4>Rawg Clone</h4>
+            <h5 className='color-2'>#API #VanillaJS #FrontEnd #HTML #CSS #Solo</h5>
+            <a href='https://rawg-eight.vercel.app/'>view</a>
+          </div>
         </SwiperSlide>
 
         <SwiperSlide className='slide'>
           <img src="src/assets/spotify.png" alt="" />
+          <div className="info-container">
+            <h4>Spotify Like Audio Player</h4>
+            <h5 className='color-2'>#API #VanillaJS #FrontEnd #HTML #CSS #Solo</h5>
+            <a href='https://spotifyy-ten.vercel.app/'>view</a>
+          </div>
         </SwiperSlide>
         
         <SwiperSlide className='slide'>
           <img src="src/assets/divesea.png" alt="" />
+          <div className="info-container">
+            <h4>Divesea NFT Marketplace</h4>
+            <h5 className='color-2'>#VanillaJS #FrontEnd #HTML #CSS #Team</h5>
+            <a href='https://dive-sea-eight.vercel.app/'>view</a>
+          </div>
         </SwiperSlide>
         
         <SwiperSlide className='slide'>
           <img src="src/assets/wezzy.png" alt="" />
+          <div className="info-container">
+            <h4>Wezzy | Weather App</h4>
+            <h5 className='color-2'>#API #VanillaJS #FrontEnd #HTML #CSS #Solo</h5>
+            <a href='https://wezzy.vercel.app/'>view</a>
+          </div>
         </SwiperSlide>
       </Swiper>
     </div>
