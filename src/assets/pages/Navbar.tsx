5a892c659cf4a4c0ee59b6b61fd915ca5dfa474b
wifv <abcogenshin@gmail.com>
2024-02-20 17:48:49 +0500

    nigga

--- src/assets/pages/Navbar.tsx
+++ src/assets/pages/Navbar.tsx
@@ -17,8 +17,8 @@ const Navbar = () => {
           <ul className={showMenu ? "active-nav-right" : "nav-right"} id='nav-right'>
               <li className="nav-item"><a href='#about'>About</a></li>
               <li className="nav-item"><a href='#skills'>Skills</a></li>
-              <li className="nav-item"><a href='#qualification'>qualification</a></li>
               <li className="nav-item"><a href='#projects'>Projects</a></li>
+              <li className="nav-item"><a href='#qualification'>Qualification</a></li>
               <li className="nav-item"><a href='#contact'>Contact</a></li>
           </ul>
           <img src={menu} alt="navigation menu" className='menu' onClick={show}/>
